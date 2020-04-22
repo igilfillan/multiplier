adfaeb82847d747fa7e98d04794bf90cddfd0ad9
IGilfillan <igwebconsulting@gmail.com>
2020-04-22 16:56:20 +0100

    Adds buttons, test passes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.css";
 
+const gridItems = count => {
+  let items = [];
+  for (let i = 1; i <= count; i++) {
+    items.push(<button key={i}>{i}</button>);
+  }
+  return items;
+};
+
 const App = () => (
   <div>
     <h2>Multiplier</h2>
+    {gridItems(144)}
   </div>
 );
 
