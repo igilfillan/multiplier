b4b72c597197716851fa4f75aff75eb2206a0a21
IGilfillan <igwebconsulting@gmail.com>
2020-04-22 16:35:31 +0100

    Adds test for header

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+test("renders learn react link", () => {
+    let wrapper = shallow(<App />);
+    expect(wrapper.find('h2').text()).toEqual('Multiplier');
 });
+
+// The application will display a grid of all the numbers from 1 to 144
+
+// When the child clicks a box then all the numbers which are a multiple of that number should be highlighted in some way.
+
+// Clicking a second time on a previously highlighted number should remove all highlighting from numbers that are multiples of the number that is clicked.
